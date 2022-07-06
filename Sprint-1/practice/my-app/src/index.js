6eea4771555a26f16294c1ca46f99945203aa502
yogjin <youngjin988@gmail.com>
2022-07-06 14:44:38 +0900

    state: 사용자와 상호작용

--- Sprint-1/practice/my-app/src/index.js
+++ Sprint-1/practice/my-app/src/index.js
@@ -3,15 +3,21 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 class Square extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: null,
+    };
+  }
   render() {
     return (
       <button
         className="square"
         onClick={() => {
-          console.log(`click ${this.props.value}`);
+          this.setState({ value: 'X' });
         }}
       >
-        {this.props.value}
+        {this.state.value}
       </button>
     );
   }
