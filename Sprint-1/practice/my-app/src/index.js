3eaa511bc877db6a283ae096093200fd264110f8
yogjin <youngjin988@gmail.com>
2022-07-07 14:43:03 +0900

    refactor: 사각형들을 만들 때 하드코딩 대신에 반복문 사용(Array Rendering)

--- Sprint-1/practice/my-app/src/index.js
+++ Sprint-1/practice/my-app/src/index.js
@@ -20,26 +20,20 @@ class Board extends React.Component {
     );
   }
 
+  renderBoard() {
+    const boardRows = [];
+    for (let i = 0; i < 3; i++) {
+      const innerCols = [];
+      for (let j = 0; j < 3; j++) {
+        innerCols.push(this.renderSquare(i * 3 + j));
+      }
+      boardRows.push(<div className="board-row">{innerCols}</div>);
+    }
+
+    return boardRows;
+  }
   render() {
-    return (
-      <div>
-        <div className="board-row">
-          {this.renderSquare(0)}
-          {this.renderSquare(1)}
-          {this.renderSquare(2)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(3)}
-          {this.renderSquare(4)}
-          {this.renderSquare(5)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(6)}
-          {this.renderSquare(7)}
-          {this.renderSquare(8)}
-        </div>
-      </div>
-    );
+    return <div>{this.renderBoard()}</div>;
   }
 }
 
