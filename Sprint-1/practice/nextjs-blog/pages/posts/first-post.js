038c330c490850dc87285db3ab11774133437686
yogjin <youngjin988@gmail.com>
2022-07-10 12:03:36 +0900

    feat: Modify metadata <title> using <Head> Component from next/head module

--- Sprint-1/practice/nextjs-blog/pages/posts/first-post.js
+++ Sprint-1/practice/nextjs-blog/pages/posts/first-post.js
@@ -1,9 +1,12 @@
+import Head from 'next/head';
 import Link from 'next/link';
 
 export default function FirstPost() {
   return (
     <>
-      <h1>First Post</h1>
+      <Head>
+        <title>First Post</title>
+      </Head>
       <h2>
         <Link href="/">
           <a>Back to home</a>
