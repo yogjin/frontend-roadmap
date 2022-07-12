f3c84bdbf3a44b949e489d3d8c2350d0a225f386
yogjin <youngjin988@gmail.com>
2022-07-12 12:32:54 +0900

    style: Polishing the Index Page

--- Sprint-1/practice/nextjs-blog/pages/index.js
+++ Sprint-1/practice/nextjs-blog/pages/index.js
@@ -2,6 +2,7 @@ import Head from 'next/head';
 import Layout, { siteTitle } from '../components/layout';
 import utilStyles from '../styles/utils.module.css';
 import Link from 'next/link';
+import Date from '../components/date';
 import { getSortedPostsData } from '../lib/posts';
 
 export async function getStaticProps() {
@@ -31,12 +32,13 @@ export default function Home({ allPostsData }) {
         <ul className={utilStyles.list}>
           {allPostsData.map(({ id, date, title }) => (
             <li className={utilStyles.listItem} key={id}>
-              {console.log(title)}
-              {title}
+              <Link href={`/posts/${id}`}>
+                <a>{title}</a>
+              </Link>
               <br />
-              {id}
-              <br />
-              {date}
+              <small className={utilStyles.lightText}>
+                <Date dateString={date} />
+              </small>
             </li>
           ))}
         </ul>
